056ac614b4b8ebf6ddc7054c23e09d45f4b9999c
anthonyruizcr <anthonyrg3124@hotmail.com>
2024-04-23 14:17:04 -0600

    Creación Proyecto
    Creación Proyecto

--- API_Caso2/API_Caso2/obj/Debug/net8.0/API_Caso2.AssemblyInfo.cs
+++ API_Caso2/API_Caso2/obj/Debug/net8.0/API_Caso2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Caso2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39f10eb68daa38f66f7469248db558938fcfc706")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Caso2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Caso2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
